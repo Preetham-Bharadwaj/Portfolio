57ca8b2b33ec844198988d6f36774adf4f484015
Preetham-Bharadwaj <preethambharadwajbr@gmail.com>
2026-04-04 12:26:09 +0530

    Added resume

---
+++ src/constants/links.ts
@@ -0,0 +1,2 @@
+export const RESUME_VIEW_LINK = "https://docs.google.com/document/d/1d48tdRcaOM3LFqqfEKBnrQVqJLJmJJ4q/preview";
+export const RESUME_DOWNLOAD_LINK = "https://docs.google.com/document/d/1d48tdRcaOM3LFqqfEKBnrQVqJLJmJJ4q/export?format=pdf";
